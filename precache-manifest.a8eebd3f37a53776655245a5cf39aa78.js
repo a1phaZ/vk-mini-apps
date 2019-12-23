3420a6cd36fdf41b85cbcf199ff273dc29decbad
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2019-12-23 14:27:06 +0500

    Updates

---
+++ precache-manifest.a8eebd3f37a53776655245a5cf39aa78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce6345651d6d2ab9f6b9ae1c33ae94c",
+    "url": "/vk-mini-apps/index.html"
+  },
+  {
+    "revision": "37a078808baace98349e",
+    "url": "/vk-mini-apps/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "4ea7415a482b0e0a3bd7",
+    "url": "/vk-mini-apps/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "37a078808baace98349e",
+    "url": "/vk-mini-apps/static/js/2.67a8fab8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vk-mini-apps/static/js/2.67a8fab8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ea7415a482b0e0a3bd7",
+    "url": "/vk-mini-apps/static/js/main.0abe0493.chunk.js"
+  },
+  {
+    "revision": "39f89162dd94cf00ab92",
+    "url": "/vk-mini-apps/static/js/runtime-main.ea509f27.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-mini-apps/static/media/persik.4e1ec840.png"
+  }
+]);
