721677f45a5d0e6ed03ffde6a5f9fb000467a0ae
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-02-06 14:29:44 +0500

    add font-size property

--- src/panels/ColoredSum.js
+++ src/panels/ColoredSum.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import prepare from "../handlers/prepare";
 
-const ColoredSum = ({sum}) => {
-	const stl = sum > 0 ? {color: '#28a745'} : {color: '#dc3545'};
+const ColoredSum = ({sum, fs}) => {
+	const stl = sum > 0 ? {color: '#28a745', fontSize: fs} : {color: '#dc3545', fontSize: fs};
 	return(
 		<span style={stl}>{prepare.sum(sum)}</span>
 	)
