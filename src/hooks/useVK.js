3033d7f0defe5af7564fc768278d353fc5c0e515
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-02-13 16:32:26 +0500

    useVK hook add

---
+++ src/hooks/useVK.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import connect from '@vkontakte/vk-connect';
+
+export default method => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false)
+  const [options, setOptions] = useState({})
+
+  const doVKFetch = (options = {}) => {
+    setOptions(options);
+    setIsLoading(true);
+  }
+
+  useEffect(() => {
+    if (!isLoading) {
+      return
+    }
+    async function fetchData() {
+			await connect.sendPromise(method, options)
+        .then(res => {
+          setResponse(res.data);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          setError(err.data);
+          setIsLoading(false)
+        });    
+		}
+    fetchData();
+  }, [isLoading]);
+
+  return [{response, error, isLoading}, doVKFetch];
+}
