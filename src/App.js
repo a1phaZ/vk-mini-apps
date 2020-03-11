e0808b08587a3353d8c52cb83ad5455e5ec30ad5
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-03-11 15:15:40 +0500

    redesign app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import connect from '@vkontakte/vk-connect';
 import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
 import prepare from "./handlers/prepare";
-import Main from "./panels/Main";
+import Main_ from "./panels/Main_";
 import useApi from "./hooks/useApi";
 import {CurrentUserContext} from "./contexts/currentUser";
 import {AppSignContext} from "./contexts/appSign";
 import fakeData from './handlers/receipts';
+import {Placeholder, Root, Panel, View, FormLayout, FormLayoutGroup, Input, Div} from "@vkontakte/vkui";
+import PanelHeader from "@vkontakte/vkui/dist/components/PanelHeader/PanelHeader";
+import ColoredSum from "./panels/ColoredSum";
+import Button from "@vkontakte/vkui/dist/components/Button/Button";
+import Authorization from "./views/Authorization";
+
 
 const App = () => {
-	const [activePanel, setActivePanel] = useState('balance');
+	const [activePanel, setActivePanel] = useState('authorization.login');
+	const [activeView, setActiveView] = useState('authorization');
 	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
 	const [qr, setQR] = useState('');
 	const [receipts] = useState(fakeData);
@@ -65,22 +72,22 @@ const App = () => {
 		setActivePanel(e.currentTarget.dataset.to);
 	};
 
-	console.log(currentUserState.isLoggedIn);
+	const changeActiveView = e => {
+		setActiveView(e.currentTarget.dataset.to);
+	};
+
+	console.log({currentUserState});
 	return (
-		<div>
-			{matchUrlParams
-				?
-					<Main
-						id={activePanel}
-						go={go}
-						qr={QR}
-						receipts={receipts}
-						popout={popout}
-					/>
-				:
-					null
-			}
-		</div>
+		<Root activeView={activeView}>
+			<View id={'authorization'} activePanel={activePanel}>
+				<Panel id={'authorization.login'}>
+					<Authorization go={go} type={'login'}/>
+				</Panel>
+				<Panel id={'authorization.registration'}>
+					<Authorization go={go} type={'registration'}/>
+				</Panel>
+			</View>
+		</Root>
 	);
 };
 
