834fa174b7e971da52a03876a8f4c02a792ca5fe
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-03-11 14:25:48 +0500

    Updates

--- precache-manifest.fb4a0afcf819fcc36c4afd230fab069f.js
+++ precache-manifest.fb4a0afcf819fcc36c4afd230fab069f.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "58cf230b7d9cf913aef0729dc23bbf55",
+    "revision": "77b2f5f8dece451faadf9b7ed2e056f6",
     "url": "/vk-mini-apps/index.html"
   },
   {
-    "revision": "37f92d5dd1f0f27379dd",
+    "revision": "1ed0af703f0b8c83ae30",
     "url": "/vk-mini-apps/static/css/2.73d444c6.chunk.css"
   },
   {
-    "revision": "3977c4f2f84b7ffd8314",
+    "revision": "5d0fa19eeccd8898f9a2",
     "url": "/vk-mini-apps/static/css/main.61c93454.chunk.css"
   },
   {
-    "revision": "37f92d5dd1f0f27379dd",
-    "url": "/vk-mini-apps/static/js/2.cc4935cd.chunk.js"
+    "revision": "1ed0af703f0b8c83ae30",
+    "url": "/vk-mini-apps/static/js/2.40c5cb6a.chunk.js"
   },
   {
     "revision": "f32f3f2739f945f6e8e72f406ee3a70f",
-    "url": "/vk-mini-apps/static/js/2.cc4935cd.chunk.js.LICENSE.txt"
+    "url": "/vk-mini-apps/static/js/2.40c5cb6a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3977c4f2f84b7ffd8314",
-    "url": "/vk-mini-apps/static/js/main.498f4869.chunk.js"
+    "revision": "5d0fa19eeccd8898f9a2",
+    "url": "/vk-mini-apps/static/js/main.d74524d3.chunk.js"
   },
   {
     "revision": "dc1c6c080e3f6b418732",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-mini-apps/precache-manifest.3bc089805c2bf2a6e89c92cfdfb3caaa.js"
+  "/vk-mini-apps/precache-manifest.fb4a0afcf819fcc36c4afd230fab069f.js"
 );
 
 self.addEventListener('message', (event) => {
