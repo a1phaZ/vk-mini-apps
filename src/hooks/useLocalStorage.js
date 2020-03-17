978a1331f300cb25c4a04f097fb11fd6e5cc16a7
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-03-17 13:32:45 +0500

    initial value fix

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export default (key, initialValue) => {
+export default (key, initialValue = '') => {
   const [value, setValue] = useState(() => {
     return localStorage.getItem(key) || initialValue;
   });
