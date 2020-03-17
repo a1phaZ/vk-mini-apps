83e3f2e7583e832b78d89aca4c332c832a04a972
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-03-17 14:57:06 +0500

    updating user profile by manually data enter

--- src/panels/Profile.js
+++ src/panels/Profile.js
@@ -1,25 +1,33 @@
-import React, {useState, useEffect, Fragment} from 'react';
+import React, {useState, useEffect, Fragment, useContext} from 'react';
 import {
 	FormLayout,
 	IOS,
 	PanelHeader,
 	platform,
 	PanelHeaderButton,
-	Group, List, InfoRow, Cell, Button
+	Group, Cell, Button, Input, FormLayoutGroup, FormStatus
 } from '@vkontakte/vkui';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 import connect from "@vkontakte/vk-connect";
 import Avatar from "@vkontakte/vkui/dist/components/Avatar/Avatar";
+import useApi from "../hooks/useApi";
+import {CurrentUserContext} from "../contexts/currentUser";
 
-const Profile = ({id, go, fetchedUser}) =>{
+const Profile = ({go, fetchedUser}) =>{
 	const [email, setEmail] = useState('');
 	const [phone, setPhone] = useState('');
 	const [name, setName] = useState('');
 	const [kktPassword, setKktPassword] = useState('');
+	const [fetchToFns, setFetchToFns] = useState(false);
+	//TODO Использовать {response, error} чтобы делать переход или показывать ошибки
+	const [, doApiFetch] = useApi(`/users/profile`);
+	const [startFetchData, setStartFetchData] = useState(false);
+	const [currentUserState] = useContext(CurrentUserContext);
 	const osName = platform();
 
 	useEffect(()=>{
+		//TODO Перейти с connect на bridge!!!!
 		connect.subscribe(({ detail: { type, data }}) => {
 			switch (type) {
 				case 'VKWebAppGetPersonalCardResult':
@@ -31,6 +39,30 @@ const Profile = ({id, go, fetchedUser}) =>{
 			}
 		});
 	}, []);
+
+	useEffect(() => {
+		setEmail(currentUserState.currentUser.email || '');
+		setName(currentUserState.currentUser.name || '');
+		setPhone(currentUserState.currentUser.phone || '');
+		setKktPassword(currentUserState.currentUser.password || '');
+	}, [currentUserState]);
+
+	useEffect(()=>{
+		if (!startFetchData) return;
+		const body = {
+			method: 'PUT',
+			update: {
+				email,
+				phone,
+				name,
+				kktPassword
+			}
+		};
+
+		doApiFetch(body);
+		setStartFetchData(false);
+	},[startFetchData, doApiFetch, email, phone, name, kktPassword]);
+
 	return(
 		<Fragment>
 			<PanelHeader
@@ -50,24 +82,60 @@ const Profile = ({id, go, fetchedUser}) =>{
 				</Cell>
 			</Group>}
 			<FormLayout>
-				<Group>
-					<List>
-						<Cell>
-							<InfoRow title='E-mail'>
-								{email ? email : 'Необходимо получить email'}
-							</InfoRow>
-						</Cell>
-						<Cell>
-							<InfoRow title='Телефон'>
-								{phone ? phone : 'Необходимо получить телефон'}
-							</InfoRow>
-						</Cell>
-					</List>
-				</Group>
-				<Button size="xl" level="2" onClick={() => {
-					connect.send("VKWebAppGetPersonalCard", {"type": ["phone", "email"]});
+				{fetchToFns && <FormStatus header="Запрос на получение пароля успешно отправлен" mode="default">
+					Пароль должен придти к вам в смс. Введите пароль в соответствующее поле. Если пароль не пришел повторите попытку позже.
+				</FormStatus>}
+				<Input
+					type={'text'}
+					top={'Имя'}
+					name={'name'}
+					value={name}
+					onChange={(e) => {setName(e.target.value)}}
+				/>
+				<FormLayoutGroup top="E-mail и Телефон" bottom="Эти данные можно получить через персональные карточки Вконтакте. Они нужны для того, чтобы получить пароль от ФНС для расшифровки чеков.">
+					<Input
+						type={'email'}
+						top={'e-mail'}
+						name={'email'}
+						placeholder={'Введите e-mail'}
+						value={email}
+						onChange={(e) => {setEmail(e.target.value)}}
+					/>
+					<Input
+						type={'phone'}
+						top={'Телефон'}
+						name={'phone'}
+						value={phone}
+						placeholder={'Введите телефон'}
+						onChange={(e) => {setPhone(e.target.value)}}
+					/>
+					<Button size="xl" onClick={() => {
+						//TODO Перейти с connect на bridge!!!!
+						connect.send("VKWebAppGetPersonalCard", {"type": ["phone", "email"]});
+					}}>
+						Получить данные из VK
+					</Button>
+				</FormLayoutGroup>
+				<FormLayoutGroup top={'Пароль от ФНС'}>
+					<Input
+						type={'number'}
+						top={'Пароль от ФНС'}
+						placeholder={'Введите пароль ФНС'}
+						name={'kktPassword'}
+						value={kktPassword}
+						onChange={(e) => {setKktPassword(e.target.value)}}
+					/>
+					<Button size="xl" onClick={() => {
+						setFetchToFns(true);
+					}}>
+						Получить пароль от ФНС
+					</Button>
+				</FormLayoutGroup>
+
+				<Button size="xl" mode="commerce" onClick={() => {
+					setStartFetchData(true);
 				}}>
-					Получить данные из VK
+					Обновить профиль пользователя
 				</Button>
 			</FormLayout>
 		</Fragment>
