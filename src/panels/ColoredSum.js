909fd34dbf7c468b58a4a2104ddc5de281395fd1
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-03-18 14:25:41 +0500

    fix color to zero value

--- src/panels/ColoredSum.js
+++ src/panels/ColoredSum.js
@@ -2,7 +2,7 @@ import React from 'react';
 import prepare from "../handlers/prepare";
 
 const ColoredSum = ({sum, fs}) => {
-	const stl = sum > 0 ? {color: '#28a745', fontSize: fs} : {color: '#dc3545', fontSize: fs};
+	const stl = sum >= 0 ? {color: '#28a745', fontSize: fs} : {color: '#dc3545', fontSize: fs};
 	return(
 		<span style={stl}>{prepare.sum(sum)}</span>
 	)
