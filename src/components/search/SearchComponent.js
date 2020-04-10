5302daad646d37e5d8c024252a5182cdf366537d
a1phaZ <pride.ots@gmail.com>
2020-04-10 14:19:49 +0500

    add dateTime into prepared array

--- src/components/search/SearchComponent.js
+++ src/components/search/SearchComponent.js
@@ -1,6 +1,7 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import {Search} from "@vkontakte/vkui";
 import Receipt from "../../panels/Receipt";
+import Group from "@vkontakte/vkui/dist/components/Group/Group";
 
 const SearchComponent = ({ receipts }) => {
   const [search, setSearch] = useState('');
@@ -14,12 +15,18 @@ const SearchComponent = ({ receipts }) => {
   useEffect(() => {
     if (!receipts) return;
     let arr = [];
+    console.time('prepare array');
     const tmp = receipts.map((receipt) => {
-      return receipt.items;
+      let dateTime = receipt.dateTime;
+      return receipt.items.map((item) => {
+        item.dateTime = dateTime;
+        return item;
+      });
     });
     tmp.forEach((item) => {
       arr.push(...item);
     });
+    console.timeEnd('prepare array');
     setSearchTarget(arr);
   }, [receipts]);
 
@@ -28,15 +35,17 @@ const SearchComponent = ({ receipts }) => {
       setSearchResult([]);
       return;
     }
-    setSearchResult(searchTarget.filter(({name}) => name.toLowerCase().indexOf(search) > -1));
+    setSearchResult(searchTarget.filter(({name}) => name.toLowerCase().indexOf(search) > -1)).sort((a, b) => b.dateTime - a.dateTime);
   }, [search, searchTarget]);
 
   return (
     <Fragment>
       <Search value={search} onChange={onSearchChange}/>
-      {
-        searchResult.length > 0 && <Receipt id={searchResult._id} dateTime={searchResult.dateTime} items={searchResult}/>
-      }
+      <Group title='Поиск'>
+        {
+          searchResult.length > 0 && <Receipt id={searchResult._id} dateTime={searchResult.dateTime} items={searchResult}/>
+        }
+      </Group>
     </Fragment>
   )
 };
