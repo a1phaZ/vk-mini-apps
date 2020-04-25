913100e23732e9996a2ec72a9131ce9aa6d758e6
a1phaZ <pride.ots@gmail.com>
2020-04-25 20:59:19 +0500

    Updates

--- precache-manifest.a44e339764ef156cbc9403c584278bda.js
+++ precache-manifest.a44e339764ef156cbc9403c584278bda.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fde072bc31e243f9f2b03360ce03c7c3",
+    "revision": "8aa3f9dcf80c9b44a679ff277f015374",
     "url": "/vk-mini-apps/index.html"
   },
   {
-    "revision": "fca5dfaf44cca4499384",
+    "revision": "25398a9dc090a0edba86",
     "url": "/vk-mini-apps/static/css/2.abebce0a.chunk.css"
   },
   {
-    "revision": "56e3574059ed2bea11ec",
+    "revision": "f537520e0193832f5477",
     "url": "/vk-mini-apps/static/css/main.842aaa3d.chunk.css"
   },
   {
-    "revision": "fca5dfaf44cca4499384",
-    "url": "/vk-mini-apps/static/js/2.f5b953f6.chunk.js"
+    "revision": "25398a9dc090a0edba86",
+    "url": "/vk-mini-apps/static/js/2.e672674c.chunk.js"
   },
   {
     "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
-    "url": "/vk-mini-apps/static/js/2.f5b953f6.chunk.js.LICENSE.txt"
+    "url": "/vk-mini-apps/static/js/2.e672674c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "56e3574059ed2bea11ec",
-    "url": "/vk-mini-apps/static/js/main.2a408ed9.chunk.js"
+    "revision": "f537520e0193832f5477",
+    "url": "/vk-mini-apps/static/js/main.f69fc618.chunk.js"
   },
   {
     "revision": "b3e15767c75ed7ff303e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-mini-apps/precache-manifest.431a3380eb29de8c318691aec8bbbb10.js"
+  "/vk-mini-apps/precache-manifest.a44e339764ef156cbc9403c584278bda.js"
 );
 
 self.addEventListener('message', (event) => {
