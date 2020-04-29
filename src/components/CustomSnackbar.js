dbc0a350d3c8977a9ff7f7300747c423a1806788
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-04-29 10:21:19 +0500

    Add CustomSnackbar to show success or error message

---
+++ src/components/CustomSnackbar.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect, useContext} from 'react';
+import Avatar from "@vkontakte/vkui/dist/components/Avatar/Avatar";
+import {Snackbar} from "@vkontakte/vkui";
+import Icon16Done from '@vkontakte/icons/dist/16/done';
+import Icon28ErrorOutline from '@vkontakte/icons/dist/28/error_outline';
+import {RouterContext} from "../contexts/routerContext";
+
+const CustomSnackBar = ({message, isError}) => {
+	const [snackbar, setSnackbar] = useState(null);
+	const [, dispatch] = useContext(RouterContext);
+	useEffect(() => {
+		setSnackbar(<Snackbar
+			layout="vertical"
+			onClose={() => {
+				dispatch({ type: 'UNSET_ERROR'});
+				setSnackbar(null);
+			}}
+			before={
+				isError
+					? <Avatar size={24} style={{backgroundColor: 'var(--destructive)'}}><Icon28ErrorOutline fill="#fff" width={18} height={18} /></Avatar>
+					: <Avatar size={24} style={{backgroundColor: 'var(--accent)'}}><Icon16Done fill="#fff" width={18} height={18} /></Avatar>
+			}
+		>
+			{message}
+		</Snackbar>)
+	}, [setSnackbar, dispatch, isError, message]);
+	return(
+		snackbar
+	)
+};
+
+export default CustomSnackBar;
