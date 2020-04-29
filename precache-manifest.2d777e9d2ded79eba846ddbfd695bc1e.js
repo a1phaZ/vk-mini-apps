112b2c06860ff03d1620c7a018e61bdb1f342622
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-04-29 12:52:16 +0500

    Updates

--- precache-manifest.2d777e9d2ded79eba846ddbfd695bc1e.js
+++ precache-manifest.2d777e9d2ded79eba846ddbfd695bc1e.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c7644acd7d2577bf42bfeebec763cf4",
+    "revision": "3baa8571a270a9151b6b5287e9a71d29",
     "url": "/vk-mini-apps/index.html"
   },
   {
-    "revision": "5fbfc7c92891bbb67bd8",
+    "revision": "7bcccfc7e7dcd1f631ce",
     "url": "/vk-mini-apps/static/css/2.abebce0a.chunk.css"
   },
   {
-    "revision": "62d9b8911270990ff0c2",
+    "revision": "4981b1a8c32e0d10fb42",
     "url": "/vk-mini-apps/static/css/main.842aaa3d.chunk.css"
   },
   {
-    "revision": "5fbfc7c92891bbb67bd8",
-    "url": "/vk-mini-apps/static/js/2.1e542568.chunk.js"
+    "revision": "7bcccfc7e7dcd1f631ce",
+    "url": "/vk-mini-apps/static/js/2.c46c5cbb.chunk.js"
   },
   {
     "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
-    "url": "/vk-mini-apps/static/js/2.1e542568.chunk.js.LICENSE.txt"
+    "url": "/vk-mini-apps/static/js/2.c46c5cbb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "62d9b8911270990ff0c2",
-    "url": "/vk-mini-apps/static/js/main.8ea444ad.chunk.js"
+    "revision": "4981b1a8c32e0d10fb42",
+    "url": "/vk-mini-apps/static/js/main.1cdf1453.chunk.js"
   },
   {
     "revision": "b3e15767c75ed7ff303e",
