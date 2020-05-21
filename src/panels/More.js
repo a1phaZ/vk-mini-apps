4ed4250a2589d03ca71026fe34a473a62fdf3aa5
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-05-21 14:57:40 +0500

    catalog menu item

--- src/panels/More.js
+++ src/panels/More.js
@@ -1,15 +1,29 @@
-import React, {Fragment} from 'react';
-import {PanelHeader, Placeholder} from '@vkontakte/vkui';
+import React, {Fragment, useContext} from 'react';
+import {Group, PanelHeader, Header, SimpleCell} from '@vkontakte/vkui';
+import Icon28ListOutline from '@vkontakte/icons/dist/28/list_outline';
+import {RouterContext} from "../contexts/routerContext";
 
 const More = () => {
+	const [, dispatch] = useContext(RouterContext);
+	const onCellClick = (e) => {
+		dispatch({type: 'SET_PANEL', payload: { panel: e.currentTarget.dataset.panel }});
+	}
 	return (
 		<Fragment>
 			<PanelHeader>
 				Дополнительно
 			</PanelHeader>
-			<Placeholder>
-				Раздел находится в разработке
-			</Placeholder>
+			<Group>
+				<Header mode={'secondary'}>Дополнительно</Header>
+				<SimpleCell
+					data-panel={'catalog'}
+					expandable
+					before={<Icon28ListOutline />}
+					onClick = {onCellClick}
+				>
+					Справочник
+				</SimpleCell>
+			</Group>
 		</Fragment>
 	)
 };
