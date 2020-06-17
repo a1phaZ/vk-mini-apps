7932ef78c337135f8b78fedc7412268021fe0998
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-06-17 16:02:43 +0500

    delete console.log

--- src/handlers/history.js
+++ src/handlers/history.js
@@ -1,6 +1,5 @@
 // import { createHashHistory } from 'history';
 export const historyPush = (view, panel) => {
 	window.history.pushState({view: view, panel: panel}, `${view}.${panel}`, `#${view}.${panel}`);
-	console.log(window.history);
 }
 // export default createHashHistory();
