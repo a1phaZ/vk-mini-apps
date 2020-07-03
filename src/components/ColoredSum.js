ced528f2bdb89a5464989c360ccbaa317439b347
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-07-03 09:10:47 +0500

    JSDoc

--- src/components/ColoredSum.js
+++ src/components/ColoredSum.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import prepare from "../handlers/prepare";
 
+/**
+ * Раскарашиваем сумму
+ * @param {number} sum - Сумма
+ * @param {string} fs
+ * @returns {React.Component}
+ * @constructor
+ */
 const ColoredSum = ({sum, fs}) => {
 	const stl = sum >= 0 ? {color: '#28a745', fontSize: fs} : {color: '#dc3545', fontSize: fs};
 	return(
